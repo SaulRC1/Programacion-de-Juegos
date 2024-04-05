ec1d695b4ce140e94b9edbe5168b8854fc0783c1
Juan 22kk <jadova16@gmail.com>
2024-04-05 22:15:41 +0200

    Añadida animacion, assets y scripts para disparo del robot

---
+++ Practicas/Practica_1/Assets/Scripts/AtaqueJugador.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class AtaqueJugador : MonoBehaviour
+{
+    [SerializeField] private float attackCoolDown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] plasmaBalls;
+    private Animator animator;
+    private MovimientoJugador movimientoJugador;
+    private float coolDownTimer = Mathf.Infinity;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        movimientoJugador = GetComponent<MovimientoJugador>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && coolDownTimer > attackCoolDown && movimientoJugador.canAttack())
+        {
+            Attack();
+        }
+
+        coolDownTimer += Time.deltaTime;
+    }
+
+    private void Attack()
+    {
+        animator.SetTrigger("attack");
+        coolDownTimer = 0;
+
+        plasmaBalls[FindFireBall()].transform.position = firePoint.position;
+        plasmaBalls[FindFireBall()].GetComponent<Proyectil>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+
+    private int FindFireBall()
+    {
+        for (int i = 0; i < plasmaBalls.Length; i++) 
+        { 
+            if (!plasmaBalls[i].activeInHierarchy)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
--- Practicas/Practica_1/Assets/Scripts/MovimientoJugador.cs
+++ Practicas/Practica_1/Assets/Scripts/MovimientoJugador.cs
@@ -1,23 +1,26 @@
 using System;
 using UnityEngine;
 
-public class NewBehaviourScript1 : MonoBehaviour
+public class MovimientoJugador : MonoBehaviour
 {
     private Rigidbody2D body;
     private Animator animator;
-    private bool grounded;
+    private BoxCollider2D boxCollider;
+    private float horizontalInput;
+    [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float velocidad;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(horizontalInput * velocidad, body.velocity.y);
 
         //Flip player when moving left-right
@@ -29,26 +32,35 @@ public class NewBehaviourScript1 : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        if (Input.GetKey(KeyCode.Space) && grounded)
+        if (Input.GetKey(KeyCode.Space) && isGrounded())
         {
             Jump();
         }
 
         //Set animator parameters
         animator.SetBool("run", horizontalInput != 0);
+        animator.SetBool("grounded", isGrounded());
     }
 
     private void Jump()
     {
         body.velocity = new Vector2(body.velocity.x, velocidad);
-        grounded = false;
+        animator.SetTrigger("jump");
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ground")
-        {
-            grounded = true;
-        }
+
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    public bool canAttack()
+    {
+        return horizontalInput == 0 && isGrounded();
     }
 }
