9e0546fc1fd2f10b6613e41610cfc5f882353797
Juan 22kk <jadova16@gmail.com>
2024-04-16 18:06:32 +0200

    Implementacion de iFrames y subrutinas para simulacion de daño

--- Practicas/Practica_1/Assets/Scripts/Health/Health.cs
+++ Practicas/Practica_1/Assets/Scripts/Health/Health.cs
@@ -1,18 +1,26 @@
 using System;
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private Boolean dead;
 
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOffFlashes;
+    private SpriteRenderer spriteRenderer;
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();   
     }
 
     public void TakeDamage(float _damage)
@@ -22,6 +30,7 @@ public class Health : MonoBehaviour
         if (currentHealth > 0)
         {
             anim.SetTrigger("hurt");
+            StartCoroutine(Invulnerability());
         } else
         {
             if (!dead) {
@@ -35,5 +44,18 @@ public class Health : MonoBehaviour
     public void addHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOffFlashes; i++)
+        {
+            spriteRenderer.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOffFlashes * 2));
+            spriteRenderer.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOffFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
     }
 }
