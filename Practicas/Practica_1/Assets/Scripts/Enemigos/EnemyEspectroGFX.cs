60b12918b40a673a69f172a812e00b2070f18f5d
Juan 22kk <jadova16@gmail.com>
2024-04-21 19:17:42 +0200

    Añadido algoritmo de pathfinding a enemigo a melee

---
+++ Practicas/Practica_1/Assets/Scripts/Enemigos/EnemyEspectroGFX.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyEspectroGFX : MonoBehaviour
+{
+    public AIPath aiPath;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(3f, 3f, 1f);
+        } 
+        else if (aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(-3f, 3f, 1f);
+        }
+    }
+}
