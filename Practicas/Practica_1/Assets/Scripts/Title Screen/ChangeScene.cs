6b48c0b24ce4606afe795391efad7aedc7b524de
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2024-04-23 00:55:09 +0200

    Añadido cambio de escena a nivel 1 al pulsar comenzar

---
+++ Practicas/Practica_1/Assets/Scripts/Title Screen/ChangeScene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneName;
+
+    public void loadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
