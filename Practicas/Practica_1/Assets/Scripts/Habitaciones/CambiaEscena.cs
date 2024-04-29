6151939dee7c3ac30d2232b9c99e0c7647fe255f
Juan 22kk <jadova16@gmail.com>
2024-04-29 10:47:20 +0200

    Arreglado el cambio de escenas y añadido paquete para implementaion arbol de comportamiento

--- Practicas/Practica_1/Assets/Scripts/Habitaciones/CambiaEscena.cs
+++ Practicas/Practica_1/Assets/Scripts/Habitaciones/CambiaEscena.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CambioEscena : MonoBehaviour
-{ 
+public class CambiaEscena : MonoBehaviour
+{
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
-        {          
-               SceneManager.LoadScene("Level 2");                              
+        {
+            SceneManager.LoadScene("Puntuation screen");
         }
     }
 }
