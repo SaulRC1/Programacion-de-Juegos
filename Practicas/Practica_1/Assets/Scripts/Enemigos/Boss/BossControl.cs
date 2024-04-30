0f6f0986744effcb70ffe532e7a9e3f73deeee49
Juan 22kk <jadova16@gmail.com>
2024-04-30 22:43:50 +0200

    Accion de esquivo del boss completada

--- Practicas/Practica_1/Assets/Scripts/Enemigos/Boss/BossControl.cs
+++ Practicas/Practica_1/Assets/Scripts/Enemigos/Boss/BossControl.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(MonoBehaviourTree))]
 public class BossControl : MonoBehaviour
 {
-    
+
     public MonoBehaviourTree monoBehaviourTree;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private Blackboard blackboard;
+    [SerializeField] private LayerMask proyectilesLayer;
+    [SerializeField] private Animator animator;
+    private System.Random rnd = new System.Random();
 
     void Reset()
     {
@@ -16,6 +21,7 @@ public class BossControl : MonoBehaviour
 
     void Update()
     {
+        playerShooting();
         monoBehaviourTree.Tick();
     }
 
@@ -25,6 +31,34 @@ public class BossControl : MonoBehaviour
         {
             monoBehaviourTree = null;
             Debug.LogWarning("Subtree should not be target of update. Select parent tree instead.", this.gameObject);
+        }
+    }
+
+    private void playerShooting()
+    {
+        RaycastHit2D hit =
+            Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.left,
+            1f, proyectilesLayer);
+
+        BoolVariable meDisparan = blackboard.GetVariable<BoolVariable>("meDisparan");
+        BoolVariable noMeDisparan = blackboard.GetVariable<BoolVariable>("noMeDisparan");
+
+        int num = rnd.Next(1, 11);
+
+        if (num == 1 )
+        {
+            if (hit.collider != null)
+            {
+                //Debug.Log("me disparan a true");
+                meDisparan.Value = true;
+                noMeDisparan.Value = false;
+            }         
+        }
+        else
+        {
+            //Debug.Log("me disparan a false");
+            meDisparan.Value = false;
+            noMeDisparan.Value = true;
         }
     }
 }
--- Practicas/Practica_1/Assets/Scripts/Enemigos/Boss/NoMeDisparanBehaviour.cs
+++ Practicas/Practica_1/Assets/Scripts/Enemigos/Boss/NoMeDisparanBehaviour.cs
@@ -2,15 +2,15 @@ using MBT;
 using UnityEngine;
 
 [AddComponentMenu("")]
-[MBTNode(name = "Examples/Me Disparan")]
-public class MeDisparanBehaviour : Leaf
+[MBTNode(name = "Examples/No Me Disparan")]
+public class NoMeDisparanBehavoiur : Leaf
 {
     [SerializeField] private Animator animator;
 
     public override NodeResult Execute()
     {
-        animator.SetBool("duck", true);
-        //Debug.Log("Me disparan");
+        animator.SetBool("duck", false);
+        //Debug.Log("No Me disparan");
         return NodeResult.success;
-    }      
+    }
 }
